ec828949def4202d8296e9194e40fd3eab1bfd15
hurradieweltgehtunter <hi@florianlenz.com>
2025-04-28 12:29:38 +0200

    .

--- scripts/setup-subdomain.sh
+++ scripts/setup-subdomain.sh
@@ -70,7 +70,7 @@ server {
         # Spezielle Behandlung für Preflight (OPTIONS)
         if (\$request_method = OPTIONS) {
             add_header Access-Control-Allow-Origin \$http_origin always;
-            # add_header Access-Control-Allow-Credentials true always;
+            add_header Access-Control-Allow-Credentials true always;
             add_header Access-Control-Allow-Methods "GET, POST, OPTIONS, PUT, DELETE" always;
             add_header Access-Control-Allow-Headers "Authorization, Content-Type, X-Requested-With" always;
             add_header Content-Length 0;
